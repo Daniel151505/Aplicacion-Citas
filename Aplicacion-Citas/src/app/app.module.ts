663126f0f0f314184aa9775916a5443d0f475e2f
Daniel151505 <daniel150520011@gmail.com>
2021-02-16 22:01:55 -0500

    Agregando Componente lista-citas

--- Aplicacion-Citas/src/app/app.module.ts
+++ Aplicacion-Citas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaCitasComponent } from './components/lista-citas/lista-citas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaCitasComponent
   ],
   imports: [
     BrowserModule,
