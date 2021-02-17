bd27fbe2b4a2dfaed070797079ab8b2a0e26ffc8
Daniel151505 <daniel150520011@gmail.com>
2021-02-16 22:03:07 -0500

    Agregando Componente crear-cita

--- Aplicacion-Citas/src/app/app.module.ts
+++ Aplicacion-Citas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaCitasComponent } from './components/lista-citas/lista-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaCitasComponent
+    ListaCitasComponent,
+    CrearCitaComponent
   ],
   imports: [
     BrowserModule,
